eb609b44990329c16f68073829baa4c85a250442
Kaanda <38444918+kaanda@users.noreply.github.com>
2024-07-09 20:16:54 -0300

    inicando o projeto e estruturando html, css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import './index.css'
+// import Teste from './components/teste'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
