b41078be759fbc79a18819eb3950d92cb60b4ca0
Kaanda <38444918+kaanda@users.noreply.github.com>
2024-07-11 16:05:35 -0300

    feat/add-dayPicker

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -8,6 +8,10 @@ const modalVariants = tv({
             primary: 'w-[540px] bg-zinc-900 rounded-xl py-5 px-6 shadow-shape space-y-5', 
             secondary: 'w-[640px] bg-zinc-900 rounded-xl py-5 px-6 shadow-shape space-y-5',
         },
+
+        size:{
+            360: 'w-[360px]',
+        }
     },
 
     defaultVariants: {
@@ -20,9 +24,9 @@ interface ModalProps extends VariantProps<typeof modalVariants>{
     children: ReactNode;
 }
 
-export function Modal ({children, variant}: ModalProps){
+export function Modal ({children, variant, size}: ModalProps){
     return (
-        <div className={modalVariants({variant})} >
+        <div className={modalVariants({variant, size})} >
             {children}
         </div>
     )
--- src/pages/create-trip/steps/destination-and-date-step.tsx
+++ src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -1,15 +1,37 @@
-import { ArrowRight, Calendar, MapPin, Settings2 } from "lucide-react";
+import { ArrowRight, Calendar, Contact, MapPin, Settings2, X } from "lucide-react";
 import { Button } from "../../../components/button";
 import { Input } from "../../../components/input";
+import { useState } from "react";
+import { Modal } from "../../../components/modal";
+import { DateRange, DayPicker } from "react-day-picker";
+import { format } from 'date-fns'
+import "react-day-picker/dist/style.css";
 
 interface DestinationAndDateStepProps {
     openGuestInput: () => void;
     closeGuestInput: () => void;
     isGuestInputOpen: boolean;
-
 }
 
-export function DestinationAndDateStep({ openGuestInput, closeGuestInput, isGuestInputOpen} : DestinationAndDateStepProps) {
+export function DestinationAndDateStep({
+    openGuestInput,
+    closeGuestInput,
+    isGuestInputOpen,
+}: DestinationAndDateStepProps) {
+
+    const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+    const [eventStartAndEndDates, setEventStartAndEndDates] = useState<DateRange | undefined>();
+    const displayedDate = eventStartAndEndDates && eventStartAndEndDates.from && eventStartAndEndDates.to ?
+         format(eventStartAndEndDates.from, "d' de 'LLL").concat(' até ').concat(format(eventStartAndEndDates.to, "d' de 'LLL")) 
+         : null;
+
+    function openDatePicker() {
+        return setIsDatePickerOpen(true);
+    }
+
+    function closeDatePicker() {
+        return setIsDatePickerOpen(false);
+    }
 
     return (
         <div className="h-16 rounded-xl pl-6 pr-4 bg-zinc-900 flex items-center shadow-shape gap-5">
@@ -19,21 +41,45 @@ export function DestinationAndDateStep({ openGuestInput, closeGuestInput, isGues
                     disabled={isGuestInputOpen}
                     type="text"
                     textSize="lg"
-                    textColor="zinc400" 
+                    textColor="zinc400"
                     placeholder="Para onde você vai?"
                 />
             </div>
 
-            <div className="flex items-center gap-2">
+            <button
+                onClick={openDatePicker}
+                disabled={isGuestInputOpen}
+                className="flex items-center gap-2 text-left"
+            >
                 <Calendar className="size-5 text-zinc-400" />
-                <Input
-                    disabled={isGuestInputOpen}
-                    type="text"
-                    textSize="lg"
-                    textColor="zinc400" 
-                    placeholder="Quando?"
-                />
-            </div>
+                <span className="text-lg text-zinc-400 w-50 flex-1">
+                    {displayedDate || 'Quando?'}
+                </span>
+            </button>
+
+            {isDatePickerOpen && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+                    <Modal variant="primary" size={360}>
+                        <div className="space-y-2">
+                            <div className="flex items-center justify-between">
+                                <h2 className="text-lg font-semibold">
+                                    Selecione a data
+                                </h2>
+                                <button type="button" onClick={closeDatePicker}>
+                                    <X className="size-5 text-zinc-400" />
+                                </button>
+                            </div>
+                        </div>
+
+                        <DayPicker 
+                            mode="range" 
+                            selected={eventStartAndEndDates} 
+                            onSelect={setEventStartAndEndDates} 
+                        />
+
+                    </Modal>
+                </div>
+            )}
 
             <div className="w-px h-6 bg-zinc-800" />
 
@@ -42,7 +88,6 @@ export function DestinationAndDateStep({ openGuestInput, closeGuestInput, isGues
                     Alterar local/data
                     <Settings2 className="size-5" />
                 </Button>
-                
             ) : (
                 <Button variant="primary" onClick={openGuestInput}>
                     Continuar
